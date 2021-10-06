9d34a7406e68f36c9cf062a4ac7199ca4dadd864
Alaa Alazzeh <aalazzeh@dons.usfca.edu>
2021-10-05 21:58:02 -0700

    Rename function per lintr suggestion

--- code/01_subset_data_to_a_state.R
+++ code/01_subset_data_to_a_state.R
@@ -9,12 +9,12 @@
 # September 16, 2021
 
 # load functions
-source("code/functions/subset_mobility_data_to_state.R")
+source("code/functions/subset_data_to_a_state.R")
 
 # test out the use of the function
 for (state in c("Montana", "Alaska", "Utah", "Colorado",
                          "Arizona")) {
-subset_mobility_data_to_state(
+subset_data_to_a_state(
 input_file_name = "data/raw_data/applemobilitytrends-2021-09-29.csv",
 state_to_subset = state)
   }
