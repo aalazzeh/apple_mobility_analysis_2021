9ef41f1217d192cb311e8ec62cf3f354adad231c
Alaa Alazzeh <aalazzeh@dons.usfca.edu>
2021-10-12 23:08:54 -0700

    Fix to actually allow wide data to convert to long

--- code/functions/wide_subset_data_to_long.R
+++ code/functions/wide_subset_data_to_long.R
@@ -11,6 +11,9 @@
 # load in dplyr
 library("dplyr")
 
+#load in tidyr
+library("tidyr")
+
 # function for subsetting
 wide_subset_data_to_long <- function(input_file_name) {
   # read in any state csv to load in the subsetted data files
@@ -18,17 +21,20 @@ wide_subset_data_to_long <- function(input_file_name) {
 
 
   # Convert the wide subsetted state data into long data using tidyr
-  long_subset_state <- tidyr::pivot_longer(subset_state_mobility_data,
-                                           cols = starts_with("202"),
-                                           names_to = "date",
-                                           values_to = "relative_mobility")
+  long_subset_state <- subset_state_mobility_data %>%
+    tidyr::pivot_longer(cols = starts_with("202"),
+                        names_to = "date",
+                        values_to = "relative_mobility")
 
 
   # write a csv to save the new long subsetted state mobility data in a new
   # output folder
-  readr::write_csv(subset_state_mobility_data, file = paste0("output/",
+  readr::write_csv(long_subset_state, file = paste0("output/",
                                                 "long_subsetted_state/",
                                                 tools::file_path_sans_ext(
                                                   basename(input_file_name)),
                                                 "_subset_state_long_data.csv"))
 }
+
+
+
